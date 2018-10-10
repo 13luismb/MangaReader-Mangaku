7703ba23317223c9830a254ebc51396e035e065c
Luis A. Acurero <13luismb@gmail.com>
2018-10-09 20:35:28 -0400

    Control de datos - v0.1
    Añadido:
    -UserModel: Modelo de usuario.
    -JacksonMapper: Transformación dinámica de POJO a JSON y viceversa usando Jackson API.

--- src/main/java/Model/UserModel.java
+++ src/main/java/Model/UserModel.java
@@ -3,12 +3,45 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Model;
+package model;
 
 /**
  *
- * @author VulcanoMachine 2.0
+ * @author kko_0
  */
 public class UserModel {
+    private String name, password, email, username;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
     
 }
--- src/main/java/util/JacksonMapper.java
+++ src/main/java/util/JacksonMapper.java
@@ -1,5 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package util;
 
-public class JacksonMapper {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.stream.Collectors;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+/**
+ *
+ * @author kko_0
+ */
+public class JacksonMapper {
+  private static ObjectMapper objMap = new ObjectMapper();
+  
+  public static <T> T jsonToPojo(HttpServletRequest request, Class clase) throws IOException{
+      return (T) objMap.readValue(request.getReader().lines().collect(Collectors.joining(System.lineSeparator())), clase);
+  }
+  
+  public static void pojoToJson (HttpServletResponse response){
+      
+  }
+  
 }
