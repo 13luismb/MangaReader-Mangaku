2f53d3d9a1dad27ea15477d01cf4f824db9b1278
Luis A. Acurero <13luismb@gmail.com>
2018-10-29 23:55:56 -0400

    Avances menores

---
+++ src/main/java/model/ChapterModel.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ChapterModel {
+    private String mangaName, mangaId, chapterNumber,chapterName,chapterLocation;
+
+    public String getMangaName() {
+        return mangaName;
+    }
+
+    public void setMangaName(String mangaName) {
+        this.mangaName = mangaName;
+    }
+
+    public String getMangaId() {
+        return mangaId;
+    }
+
+    public void setMangaId(String mangaId) {
+        this.mangaId = mangaId;
+    }
+
+    public String getChapterNumber() {
+        return chapterNumber;
+    }
+
+    public void setChapterNumber(String chapterNumber) {
+        this.chapterNumber = chapterNumber;
+    }
+
+    public String getChapterName() {
+        return chapterName;
+    }
+
+    public void setChapterName(String chapterName) {
+        this.chapterName = chapterName;
+    }
+
+    public String getChapterLocation() {
+        return chapterLocation;
+    }
+
+    public void setChapterLocation(String chapterLocation) {
+        this.chapterLocation = chapterLocation;
+    }
+}
---
+++ src/main/java/servlets/UploadServlet.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import facade.ChapterFacade;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Collection;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+/**
+ *
+ * @author Usuario
+ */
+@MultipartConfig
+@WebServlet(name = "UploadServlet", urlPatterns = {"/upload"})
+public class UploadServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UploadServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            //Hay que refactorizar este verguero y mandarlo al "ChapterFacade"
+            //También hay que traer una dirección relativa.
+            PrintWriter out = response.getWriter();
+		ChapterFacade chapter = new ChapterFacade();
+                String m = chapter.FileUpload(request);
+                switch(m){
+                    case "200":out.print("YESSS"); break;
+                    case "500":out.print("NOOOO"); break;
+                }
+	}
+	
+	private String getFileName(Part part) {
+		for (String content : part.getHeader("content-disposition").split(";")) {
+			if (content.trim().startsWith("filename")) {
+				return content.substring(content.indexOf('=') + 1).trim().replace("\"", "");
+			}
+		}
+		return null;
+	}
+}
